8ca9fc3ea0cdc30051234862678d9a1ddb3eb0fc
TANUJ BHAWARIYA <gokultanuj2005@gmail.com>
2026-06-11 01:35:57 +0530

    edited

---
+++ basics/basics1.py
@@ -0,0 +1,564 @@
+# '''print("hello")
+# print("bye")
+# print("python programming")
+# print("python",end="@@")
+# print("unit1")
+# print("python",end=" ")
+# print("idea about python")
+# print("hello",end="%%")'''
+# '''x=10
+# print(x)
+# print(type(x))
+# x="hello"
+# print(x)
+# print(type(x))
+# x=5.7
+# print(x)
+# print(type(x))'''
+# '''x=input("enter the number\n")
+# #2+5=7
+# #"2"+"5"=25
+# print(type(x))'''
+
+# '''a=int(input("enter the no.\n"))
+# b=int(input("enter the no.\n"))
+# c=a+b
+# print("sum",c)
+# print(type(a))'''
+
+# '''a=float(input("enter the no.\n"))
+# b=float(input("enter the no.\n"))
+# c=a+b
+# print("sum=",c)'''
+
+# '''a=int(input("enter the no.\n"))
+# b=float(input("enter the no.\n"))
+# c=a+b
+# print("sum=",c)'''
+
+# '''a=int(input("enter the no.\n"))
+# b=float(input("enter the no.\n"))
+# c=a-b
+# print("sub=",c)'''
+
+# '''a=int(input("enter the no.\n"))
+# b=float(input("enter the no.\n"))
+# c=a*b
+# print("product=",c)'''
+
+# '''a=int(input("enter the no.\n"))
+# b=float(input("enter the no.\n"))
+# c=float(input("enter the no.\n"))
+# d=a+b+c
+# print("sum=",d)'''
+
+# '''name=input("enter your name: ")
+# age=int(input("enter your age: "))
+# print("hello," + name + "! you're " + str(age) + " years old.")'''
+
+# '''age=24
+# print(type(age))
+# age="twenty-four"
+# print(type(age))
+# age=age+"5"
+# age=age+5'''
+
+# '''#swapping of two no.s without the help of third no
+# a=int(input("enter first no.\n"))#5
+# b=int(input("enter second no.\n"))#6
+# print("value of a & b before swapping are:")
+# print("a")
+# print("b")
+# a=a+b#5+6=11
+# b=a-b#11-6=5
+# a=a-b#11-5=6
+# print("the value of a & b after swapping are:")
+# print("value of a:",a)
+# print("value of b:",b)'''
+
+# '''#swapping of two no.s with the help of third no
+# a=int(input("enter first no.\n"))#5
+# b=int(input("enter second no.\n"))#6
+# print("value of a & b before swapping are:")
+# print("a")
+# print("b")
+# c=a#c=5
+# a=b#a=6
+# b=c#b=5
+# print("the value of a & b after swapping are:")
+# print("value of a:",a)
+# print("value of b:",b)'''
+
+# #explicit type conversion
+# '''s="10010"
+# z=int(s,2)
+# print(z)
+# w=float(z)
+# print(w)'''
+
+# #implicit type conversion
+# '''a=10.5
+# print(type(a))
+# b=20
+# print(type(b))
+# c=a+b
+# print(type(c))
+# print(c)'''
+
+# '''#concatenation
+# a="hello"
+# b="hi"
+# print(a+b)
+
+# #add/sum
+# a=2
+# b=3
+# print(a+b)'''
+
+# a=5
+# print("type of a:", type(a))
+# b=5.0
+# print("type of b:", type(b))
+# c=a+4j
+# print("type of c:", type(c))
+
+# str1 = "hello"
+# str2 = "hi"
+# print(str1, "is of type", type(str1))
+# print(str2, "is of type", type(str2))
+
+
+# str1="hellostudents"
+# str2="python"
+# print(str1[0:2])
+# print(str2[2:4])
+# print(str1[3])
+# print(str2[3])
+# print(str1*2)
+# print(str2*3)
+# print(str1+str2)
+
+
+# l1=[1,3,4,5]
+# l2=[2,3,4,"hello"]
+# print(l1)
+# print(type(l1))
+# #indexing
+# print(l1[1])
+# print(l1[-1])
+# #slicing
+# print(l1[0:3])
+# print(l1[0:])
+# #concatenation
+# print(l1+l2)
+# #repetition
+# print(l1*2)
+
+
+
+# tup1 = (1,2,3,4)
+# print(type(tup1))
+# tup2 = ("hello",2.3,4,3,4)
+# print(tup1[1])
+# print(tup2[-1])
+# print(tup1[1:3])
+# print(tup1+tup2)
+# print(tup1*2)
+# tup1[1]=6
+
+
+# s1={"sectionA":"good","sectionB":"excellent","sectionC":"good"}
+# print(s1)
+# print(type(s1))
+# print(s1.keys())
+# print(s1.values())
+# print(s1["sectionA"])
+
+
+# a=True
+# print(a)
+# print(type(a))
+# b=False
+# print(b)
+# print(type(b))
+
+
+# s={"banana","grapes","papita","mango","mango"}
+# print(s)
+# print(type(s))
+# s2={"1","hello",2.5}
+# print(s2)
+# s[1]="bye"
+# print(s)
+
+
+# a=float(input("enter the side of square\n"))
+# area=a*a
+# perimeter=4*a
+# print("area of square is: ",area)
+# print("perimater of square is: ",perimeter)
+
+
+# a=int(input("enter the no.\n"))
+# print(type(a))
+
+
+# print("hello")
+# print("bye")
+# a=input("enter the no.\n")
+# print(hi)
+
+
+# print("hi");print("bye");print("hello")
+
+#print("hi")print("bye");print("hello")
+
+# print("hi")
+# print("bye");print("hello")
+
+
+# x=10
+# y=3
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x**y)
+# print(x/y)
+# print(x//y)
+# print(x%y)
+# print(10%3)
+# print(7//3)
+# print(7/3)
+
+
+# x=10
+# y=3
+# print(x<y)
+# print(x<=y)
+# print(x>y)
+# print(x>=y)
+# print(x==y)
+# print(x !=y)
+# print(10==10)
+# print(10 !=10)
+# print(10 !=11)
+
+# x=10>3
+# y=3<9
+# print("x and y", x and y)
+# print("x or y", x or y)
+# print("not x",not(x))
+
+
+#bitwise operators-doubt
+
+
+# l1=[1,2,3,4]
+# b=2
+# if b in l1:
+#     print("hello")
+# else:
+#     print("bye")
+
+
+# l1=[1,2,3,4]
+# b=2
+# if b not in l1:
+#     print("hello")
+# else:
+#     print("bye")
+
+
+# l1=(1,2,3,4)
+# b=2
+# if b in l1:
+#     print("hello")
+# else:
+#     print("bye")
+
+
+# l1=(1,2,3,4)
+# b=2
+# if b not in l1:
+#     print("hello")
+# else:
+#     print("bye")
+
+
+# s={"classa":3,"classb":6}
+# b="classb"
+# if b in s:
+#     print("hello")
+#     print("hi")
+#     print("python")
+# else:
+#     print("bbye")
+#     print("hi")
+#     print("bye")
+
+
+# s={"classa":3,"classb":6}
+# b=3
+# if b in s:
+#     print("hello")
+#     print("hi")
+#     print("python")
+# else:
+#     print("bbye")
+#     print("hi")
+#     print("bye")
+
+
+# s={"classa":3,"classb":6,3:6}
+# b=3
+# if b in s:
+#     print("hello")
+#     print("hi")
+#     print("python")
+# else:
+#     print("bbye")
+#     print("hi")
+#     print("bye")
+
+
+# s={"classa":3,"classb":6,3:6}
+
+# if 3 in s:
+#     print("hello")
+#     print("hi")
+#     print("python")
+# else:
+#     print("bbye")
+#     print("hi")
+#     print("bye")
+
+
+# s={"classa":3,"classb":6}
+
+# if 3 in s:
+#     print("hello")
+#     print("hi")
+#     print("python")
+# else:
+#     print("bbye")
+#     print("hi")
+#     print("bye")
+
+
+# #variable
+# x=2
+# y=2
+# print(type(x))
+# print(type(y))
+# print(id(x))
+# print(id(y))
+# print(x is y)
+# print(x is not y)
+
+# x=2
+# y=3
+# print(type(x))
+# print(type(y))
+# print(id(x))
+# print(id(y))
+# print(x is y)
+# print(x is not y)
+
+
+# x=[1,2,3,4]
+# y=[1,2,3,4]
+# print(type(x))
+# print(type(y))
+# print(id(x))
+# print(id(y))
+# print(x is y)
+# print(x is not y)
+
+# x=(1,2,3,4)
+# y=(1,2,3,4)
+# print(type(x))
+# print(type(y))
+# print(id(x))
+# print(id(y))
+# print(x is y)
+# print(x is not y)
+
+# x="hello"
+# y="hello"
+# print(type(x))
+# print(type(y))
+# print(id(x))
+# print(id(y))
+# print(x is y)
+# print(x is not y)
+
+
+# def a():
+#     y=5
+#     print(y)
+# a()
+
+# def a():
+#     y=5
+#     print(y)
+
+# def a():
+#     y=5
+#     print("inside function",y)
+
+# def a():
+#     y=5
+#     print("inside function",y)
+# a()
+# print("outside function",y)
+
+# y=5
+# def a():
+#     print("inside function",y)
+# a()
+# print("outside function",y)
+
+# y=5
+# def a():
+#     print("inside function",y)
+# def b():
+#     print("inside function",y)
+# a()
+# b()
+# print("outside function",y)
+
+# y=5
+# def a():
+#     print("inside function",y)
+# def b():
+#     x=12
+#     print("inside function",y)
+# a()
+# b()
+# print("outside function",y)
+
+# y=5
+# def a():
+#     print("inside function",y)
+# def b():
+#     x=12
+#     print("inside function",y)
+# a()
+# b()
+# print("outside function",x)
+
+# y=5
+# def a():
+#     print("inside function",y)
+#     print("outside function",x)
+# def b():
+#     x=12
+#     print("inside function",y)
+# a()
+# b()
+
+# y=10
+# def a():
+#     y=20
+#     print("inside function",y)
+# def b():
+#     print("inside function",y)
+# a()
+# b()
+
+# print(20*3+5)
+
+# print(20/5*3//2)
+# print(4*3//2)
+# print(12//2)
+# print(9)
+
+# print(20//2*5+6-3)
+# print(10*5+6-3)
+# print(50+6-3)
+# print(56-3)
+# print(53)
+
+
+# x=6
+# print(x)
+# x=x+5
+# print(x)
+
+# x=6
+# print(x)
+# x+=5
+# print(x)
+
+# x=6
+# print(x)
+# x=x-5
+# print(x)
+
+# x=6
+# print(x)
+# x-=5
+# print(x)
+
+# x=6
+# print(x)
+# x=x+5
+# print(x)
+# x=x-5
+# print(x)
+# x=x*9
+# print(x)
+# x=x/2
+# print(x)
+
+# x=6
+# print(x)
+# x+=5
+# print(x)
+# x-=5
+# print(x)
+# x*=9
+# print(x)
+# x/=2
+# print(x)
+
+# print(2**3**2)#right to left
+# #2**9
+# print(2**9)
+
+# print(2<<3<<2)#leftt to right
+# #2*2^3=16
+# print(2*2^3)
+# print(16<<2)
+# print(16*2^2)
+# print(64)
+
+# print(3>>1>>2)
+# #3//2=1
+# #1>>2 = 1//2*2 = 1//4 = 0
+# print(3>>1)
+# print(1>>2)
+
+# print(2<3)
+# print(2<3<3)
+# print(2<3<3<7)
+# #2<3 true
+# #3<3 false
+# #3<7 false
+# print(3==3==3)
+# print(3!=3!=3)
+# print(3!=3==3)
+
+
+# print(2 or 3)
+# print(0 or 3)
+# print(0 or 3 or 6)
+# print(0 or 0 or 6)
+
+
+# print(1 and 0 and 6)
+# print(1 and 7 and 6)
+
+
+print(not(2))
+print(not(not(2)))
